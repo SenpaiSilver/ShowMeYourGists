354b7fd289e58632a7608c65e13c08b0d8c26817
SenpaiSilver <silvermesprims@gmail.com>
2014-12-31 16:30:36 +0100

    Added .php plugin for Wordpress.

---
+++ showmeyourgists.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @package ShowMeYourGists
+ * @version 0.5
+ */
+/*
+Plugin Name: Show Me Your Gists
+Plugin URI: http://senpaisilver.com/
+Description: How about embedding esealy Gists ?<br />To do so just paste the Gist link on a single line.
+Author: SenpaiSilver
+Version: 0.5
+Author URI: http://senpaisilver.com/
+*/
+
+function embedGistsInPosts($post) {
+	$pat = '/<p>(https?:)\/\/gist.github.com\/(.*)\/(.*)<\/p>/mi';
+	$sub = '<script src="$1//gist.github.com/$2/$3.js"></script>';
+	$post = preg_replace($pat, $sub, $post);
+	echo($post);
+}
+
+add_action('the_content', 'embedGistsInPosts');
+
+?>
